6aab8d5fbb11fb4de5fee6a1782b1588d69f19b8
Jonathan Banda <bandajonathan07@gmail.com>
2025-10-05 17:38:40 -0500

    More changes to the .hpp file

--- parser.hpp
+++ parser.hpp
@@ -1,10 +1,28 @@
-#include <iostream>;
+#include <iostream>
+#include <ctype.h>
 
+// Variables
+int charClass;
+char lexeme [100];
+char nextChar;
+int lexLen;
+int token;
+int nextToken;
+FILE *in_fp, *fopen();
+
+//Function declarations
+void addChar();
+void getChar();
+void getNonBlank();
+int lex();
+
+// Tokens
 #define LETTER 0
 #define DIGIT 1
 #define UNKNOWN 99
 
-#define IDENT;
+#define INT_LIT 10
+#define IDENT 11
 
 #define ADD_OP 40
 #define SUB_OP 41
@@ -18,37 +36,3 @@
 
 #define OP_PAREN 50
 #define CL_PAREN 51
-
-/*
-Global declarations
-Variables 
-int charClass;
-char lexeme [100];
-char nextChar;
-int lexLen;
-int token;
-int nextToken;
-FILE *in_fp, *fopen();
-
-Function declarations
-void addChar();
-void getChar();
-void getNonBlank();
-int lex();
-
-Character classes
-#define LETTER 0
-#define DIGIT 1
-#define UNKNOWN 99
-
-Token codes
-#define INT_LIT 10
-#define IDENT 11
-#define ASSIGN_OP 20
-#define ADD_OP 21
-#define SUB_OP 22
-#define MULT_OP 23
-#define DIV_OP 24
-#define LEFT_PAREN 25
-#define RIGHT_PAREN 26
-*/
