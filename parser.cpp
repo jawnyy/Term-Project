9de6007551cedc2c7243f1e11a7f39fc581f3568
Jonathan Banda <bandajonathan07@gmail.com>
2025-10-12 16:17:46 -0500

    Does not work, fix.

--- parser.cpp
+++ parser.cpp
@@ -46,23 +46,17 @@ int main() {
     int i = 1;
 
     if (!inputFile) {
-        cerr << "Error opening file!" << endl;
+        cout << "Error opening file!" << endl;
     } else {
-        char ch;
-        while (inputFile.get(ch)) {
-            if (ch == '\n') {
-                cout << "New line at line " << i << endl;
-                i++;
-            }
-            cout << ch << " at line " << i << endl;
-        }
+        getchar();
+        do {
+            lex();
+        } while (nextToken != EOF);
 
-        // getChar();
-        // do {
-        //     lex();
-        // } while (nextToken != EOF);
-        inputFile.close();
+        //inputFile.close();
     }
+
+    inputFile.close();
 
     // if (inputFile.is_open()) {
     //     cout << "Opened file" << endl;
@@ -80,3 +74,117 @@ int main() {
 
     return 0;
 }
+
+int lookup(char ch) {
+    switch (ch) {
+        case '(':
+            addChar();
+            nextToken = LEFT_PAREN;
+            break;
+        case ')':
+            addChar();
+            nextToken = RIGHT_PAREN;
+            break;
+        case '+':
+            addChar();
+            nextToken = ADD_OP;
+            break;
+        case '-':
+            addChar();
+            nextToken = SUB_OP;
+            break;
+        case '*':
+            addChar();
+            nextToken = MULT_OP;
+            break;
+        case '/':
+            addChar();
+            nextToken = DIV_OP;
+            break;
+        default:
+            addChar();
+            nextToken = EOF;
+            break;
+    }
+    return nextToken;
+}
+
+/*****************************************************/
+/* addChar - a function to add nextChar to lexeme */
+void addChar() {
+    if (lexLen <= 98) {
+        lexeme[lexLen++] = nextChar;
+        lexeme[lexLen] = 0;
+    }
+    else
+        cout << "Error - lexeme is too long \n" << endl;
+}
+
+/*****************************************************/
+/* getChar - a function to get the next character of
+   input and determine its character class */
+void getChar() {
+    if ((nextChar = inputFile.get()) != EOF) {
+        if (isalpha(nextChar))
+            charClass = LETTER;
+        else if (isdigit(nextChar))
+            charClass = DIGIT;
+        else 
+            charClass = UNKNOWN;
+    }
+    else
+        charClass = EOF;
+}
+
+/*****************************************************/
+/* getNonBlank - a function to call getChar until it
+   returns a non-whitespace character */
+void getNonBlank() {
+    while (isspace(nextChar))
+        getChar();
+}
+
+/*****************************************************/
+/* lex - a simple lexical analyzer for arithmetic
+   expressions */
+int lex() {
+    lexLen = 0;
+    getNonBlank();
+    switch (charClass) {
+        /* Parse identifiers */
+        case LETTER:
+            addChar();
+            getChar();
+            while (charClass == LETTER || charClass == DIGIT) {
+                addChar();
+                getChar();
+            }
+            nextToken = IDENT;
+            break;
+        /* Parse integer literals */
+        case DIGIT:
+            addChar();
+            getChar();
+            while (charClass == DIGIT) {
+                addChar();
+                getChar();
+            }
+            nextToken = INT_LIT;
+            break;
+        /* Parentheses and operators */
+        case UNKNOWN:
+            lookup(nextChar);
+            getChar();
+            break;
+        /* EOF */
+        case EOF:
+            nextToken = EOF;
+            lexeme[0] = 'E';
+            lexeme[1] = 'O';
+            lexeme[2] = 'F';
+            lexeme[3] = 0;
+            break;
+    } /* End of switch */
+    cout << "Next token is: " << nextToken << ", Next lexeme is " << lexeme << endl;
+    return nextToken;
+} /* End of function lex */
