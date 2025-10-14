240060b470ca921742e57e4eba681aa68d8750d1
Jonathan Banda <bandajonathan07@gmail.com>
2025-10-13 19:43:23 -0500

    Finished the lexical analyzer I think.

--- parser.cpp
+++ parser.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <cctype>
+#include <map>
 using namespace std;
 
 int charClass;
@@ -23,16 +24,28 @@ int lex();
 #define UNKNOWN 99
 
 /* Token codes */
-#define INT_LIT 10
-#define IDENT 11
+#define PROGRAM 10
+#define DECL_SEC 11
+#define DECL 12
+#define ID_LIST 13
+#define ID 14
+#define OPERAND 15
 
-#define ASSIGN_OP 20
-#define ADD_OP 21
-#define SUB_OP 22
-#define MULT_OP 23
-#define DIV_OP 24
-#define LEFT_PAREN 25
-#define RIGHT_PAREN 26
+#define STMT_SEC 20
+#define STMT 21
+
+#define IFSTMT 30
+#define WHILESTMT 31
+#define INPUT 32
+#define OUTPUT 33
+
+#define ASSIGN 40
+#define EXPR 41
+#define FACTOR 42 
+#define COMP 43
+
+#define NUM 50
+#define TYPE 51
 
 int main() {
     // Uncomment this later, for easier testing purposes.
@@ -48,63 +61,80 @@ int main() {
     if (!inputFile) {
         cout << "Error opening file!" << endl;
     } else {
-        getchar();
+        getChar();
         do {
             lex();
         } while (nextToken != EOF);
 
-        //inputFile.close();
+        inputFile.close();
     }
-
-    inputFile.close();
-
-    // if (inputFile.is_open()) {
-    //     cout << "Opened file" << endl;
-    //     string line;
-    //     int i = 1;
-    //     while (getline(inputFile, line)) {
-    //         cout << line << "--------line #" << i << endl;
-    //         i++;
-    //     }
-    //     inputFile.close();
-    // } else {
-    //     cerr << "Error opening file!";
-    //     // error
-    // }
 
     return 0;
 }
 
 int lookup(char ch) {
     switch (ch) {
-        case '(':
+        case ',':
             addChar();
-            nextToken = LEFT_PAREN;
+            nextToken = DECL_SEC;
             break;
+        case ':':
+            addChar();
+            nextToken = DECL;
+            break;
+        case ';':
+            addChar();
+            break;
+        case '=':
+            addChar();
+            nextToken = ASSIGN;
+            break;
+        case '(':
         case ')':
             addChar();
-            nextToken = RIGHT_PAREN;
+            nextToken = OPERAND;
+            break;
+        case '<':
+        case '>':
+            addChar();
+            nextToken = COMP;
             break;
         case '+':
-            addChar();
-            nextToken = ADD_OP;
-            break;
         case '-':
             addChar();
-            nextToken = SUB_OP;
+            nextToken = EXPR;
             break;
         case '*':
-            addChar();
-            nextToken = MULT_OP;
-            break;
         case '/':
             addChar();
-            nextToken = DIV_OP;
+            nextToken = FACTOR;
             break;
         default:
             addChar();
             nextToken = EOF;
             break;
+    }
+    return nextToken;
+}
+// program, begin, end, if, then, else, input, output, int, while, loop.
+int id_lookup(string word) {
+    if (word == "program") {
+        nextToken = PROGRAM;
+    }
+    else if (word == "input") {
+        nextToken = INPUT;
+    } 
+    else if (word == "output") {
+        nextToken = OUTPUT;
+    }
+    else if (word == "int" || word == "float" || word == "double"){
+        nextToken = TYPE;
+    }
+    else if (word == "begin" || word == "end" || word == "if" || word == "then"
+    || word == "else" || word == "while" || word == "loop") {
+        nextToken = ID_LIST;
+    } else {
+        nextToken = ID;
     }
     return nextToken;
 }
@@ -124,7 +154,8 @@ void addChar() {
 /* getChar - a function to get the next character of
    input and determine its character class */
 void getChar() {
-    if ((nextChar = inputFile.get()) != EOF) {
+    nextChar = inputFile.get();
+    if (inputFile.good()) {
         if (isalpha(nextChar))
             charClass = LETTER;
         else if (isdigit(nextChar))
@@ -159,7 +190,8 @@ int lex() {
                 addChar();
                 getChar();
             }
-            nextToken = IDENT;
+            id_lookup(string(lexeme));
+            //nextToken = IDENT;
             break;
         /* Parse integer literals */
         case DIGIT:
@@ -169,7 +201,7 @@ int lex() {
                 addChar();
                 getChar();
             }
-            nextToken = INT_LIT;
+            nextToken = NUM;
             break;
         /* Parentheses and operators */
         case UNKNOWN:
@@ -188,3 +220,20 @@ int lex() {
     cout << "Next token is: " << nextToken << ", Next lexeme is " << lexeme << endl;
     return nextToken;
 } /* End of function lex */
+
+
+/* Semantic Analyzer Components */
+
+// Symbol table entry structure
+struct SymbolInfo {
+    string name;
+    string type;      // "int", "float", "double"
+    bool initialized;
+    int scope;        // For nested scopes if needed
+};
+
+// Symbol table
+map<string, SymbolInfo> symbolTable;
+
+// Semantic error tracking
+vector<string> semanticErrors;
